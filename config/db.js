a7c02d7f801b9c67875cd4ad01e33bdb6634330d
dedeepyamaddukuri <dedeepya.maddukuri24@sasi.ac.in>
2026-07-13 15:45:16 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,9 +1,12 @@
 const mongoose = require('mongoose');
 const dns = require("dns")
 dns.setServers(["8.8.8.8","8.8.4.4"])
-const dbURI = 'mongodb+srv://kusnuthshaik24_db_user:kusnuth@20@cluster0.umvpmlg.mongodb.net/kusnuth?appname=Cluster0'; 
 
 const connectDB = () => {
+  try {
+    
+    await mongoose.connect("mongodb+srv://dedeepyamaddukuri24_db_<user:deepumongodb>@cluster0.zaqdku6.mongodb.net/dedeepya?appname=cluster0");
+  
   mongoose.connect(dbURI)
     .then(() => console.log('Connected to MongoDB successfully!'))
     .catch((err) => console.error('MongoDB connection error:', err));
